c27d28d20de9ad12d28cb4c62f4d5f0f0d01865a
kushalzone <kushalzone@gmail.com>
2017-05-22 15:57:47 -0500

    Created a compound word ending extended version to be used for Romanization

---
+++ NepaliNLPWeb/src/com/icodejava/research/nlp/domain/CompoundWordEndingExtended.java
@@ -0,0 +1,30 @@
+package com.icodejava.research.nlp.domain;
+/**
+ * This is extended compound word endings and are intended to be used for destemming during romanization only.
+ * @author paudyals
+ *
+ */
+public enum CompoundWordEndingExtended {
+    
+    
+    KARAN("करण"),
+    BIGYA("विज्ञ"),
+    KARTA("कर्ता"),
+    BADI("बादी"),
+    BAD("बाद"),
+    BAN("वाण"),
+    DUT("दूत"),
+    CHHAM("छाम"); 
+
+    private String nepaliWordEnding;
+
+    CompoundWordEndingExtended(String nepaliWordEnding) {
+
+        this.nepaliWordEnding = nepaliWordEnding;
+    }
+
+    public String getNepaliWordEnding() {
+
+        return nepaliWordEnding;
+    }
+}
